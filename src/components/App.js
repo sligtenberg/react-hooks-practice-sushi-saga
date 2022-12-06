ab60c260956ac19e20524d659e3671069351cd5d
stephen ligtenberg <bountyhunter24@gmail.com>
2022-12-06 11:57:49 -0500

    display sushis

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiData, setSushiData] = useState([])
+
+  useEffect(() => {
+    fetch(API)
+    .then(r => r.json())
+    .then(data => {
+      console.log(data)
+      setSushiData(data)
+    })
+  }, [])
+  
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushiData={sushiData}/>
       <Table />
     </div>
   );
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushiData }) {
+  const [index, setIndex] = useState(0)
+
+  const displayedSushi = sushiData.filter(sushi => {
+    return (sushi.id > index && sushi.id < index + 5) ? true : false
+  })
+
+  const sushiComponents = displayedSushi.map(sushi => <Sushi key={sushi.id} sushi={sushi} />)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {sushiComponents}
       <MoreButton />
     </div>
   );
